8d377f7fa3fff0945432afeddaa1a1a77243147e
Sunny Chan <sunny@syhc.ca>
2013-10-14 16:57:29 -0400

    Add WFA Project
    Add .gitignore
    Add WFA (Visual C#) project

---
+++ HealthInformaticSystem/Database.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInformaticSystem
+{
+    class Database
+    {
+        private SqlConnection conn;
+        
+        public Database()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>the ms sql connection</returns>
+        public SqlConnection getConnection()
+        {
+            if (conn == null)
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "SUNNY-OMEGA"; // <-- change this to your ms sql server
+                builder.InitialCatalog = "Giberson";
+                builder.IntegratedSecurity = true;
+
+                conn = new SqlConnection(builder.ConnectionString);
+            }
+            return conn;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="SqlStatement">sql statement</param>
+        /// <param name="parameters">dictionary with the "@key" and it "values"</param>
+        /// <returns>sql result in datatable format</returns>
+        public DataTable getData(string SqlStatement, Dictionary<String, String> parameters)
+        {
+            DataTable table = new DataTable();
+            
+            SqlConnection conn = getConnection();                
+            conn.Open();
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = SqlStatement;
+            foreach(KeyValuePair<String, String> p in parameters)
+            {
+                cmd.Parameters.Add(new SqlParameter(p.Key, p.Value));
+            }
+            cmd.CommandType = CommandType.Text;
+            
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                table.Load(reader);
+            }
+
+            conn.Close();
+            return table;
+        }
+    }
+}

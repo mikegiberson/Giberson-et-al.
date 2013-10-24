652b10fd49428a4b850b47f6408517985e107a11
LovuDhiraj Sharma <lovudsharma@yahoo.com>
2013-10-24 00:25:42 -0400

    Create PatientInsurance.cs

---
+++ MedicalInformationManagementSystem/models/PatientInsurance.cs
@@ -0,0 +1,12 @@
+class PatientInsurance
+{
+    public PatientInsurance()
+    {
+    }
+
+    public int patientID          { get; private set; }
+    public string timestamp       { get; private set; }
+    public string type            { get; private set; }
+    public string accountNumber   { get; private set; }
+    public string expiryDate      { get; private set; }
+}

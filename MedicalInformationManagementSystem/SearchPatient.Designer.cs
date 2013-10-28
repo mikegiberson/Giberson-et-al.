e39348c1c0db08edd5052bc644ede467012ad749
LovuDhiraj Sharma <lovudsharma@yahoo.com>
2013-10-28 14:41:44 -0400

    Search patient database script and gui redirecting to view demography of the searched patient, allows updating it

---
+++ MedicalInformationManagementSystem/SearchPatient.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace HealthInformaticSystem
+{
+    partial class SearchPatient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.patientSearch = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(12, 23);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(139, 15);
+            this.lblResult.TabIndex = 0;
+            this.lblResult.Text = "Search Patient by Id:";
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelect.Location = new System.Drawing.Point(163, 49);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(109, 41);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Update Patient Demography";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // patientSearch
+            // 
+            this.patientSearch.Location = new System.Drawing.Point(148, 23);
+            this.patientSearch.Name = "patientSearch";
+            this.patientSearch.Size = new System.Drawing.Size(124, 20);
+            this.patientSearch.TabIndex = 2;
+            // 
+            // SearchPatient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.patientSearch);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lblResult);
+            this.Name = "SearchPatient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SearhPatient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.TextBox patientSearch;
+    }
+}
+

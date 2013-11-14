1877eff6ddfd100f5a1e1321c6cb0116a47d3621
Sunny Chan <sunny@syhc.ca>
2013-11-14 10:50:45 -0500

    Fix collisions

--- MedicalInformationManagementSystem/Properties/Settings.Designer.cs
+++ MedicalInformationManagementSystem/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace HealthInformaticSystem.Properties {
+namespace MedicalInformationManagementSystem.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]

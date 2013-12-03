534b1e8e7598597ddcfb26e08bc2ea57cce4717f
LovuDhiraj Sharma <lovudsharma@yahoo.com>
2013-12-02 19:15:22 -0500

    Unit Test Search Patient By ID [61224578]

---
+++ MedicalInformationManagementSystem/UnitTestProject/TestSearchPatient.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MedicalInformationManagementSystem;
+using MIMS;
+using MedicalInformationManagementSystem.Forms;
+using System.Data;
+
+
+namespace UnitTestProject1
+{
+    /// <summary>
+    /// Summary description for SearchPatient
+    /// </summary>
+    [TestClass]
+    public class TestSearchPatient
+    {
+        [TestMethod]
+        public void TestCreatePatientSearchObject()
+        {
+            MedicalInformationManagementSystem.SearchPatient search = new SearchPatient();
+            Assert.IsNotNull(search);
+                  }
+
+        [TestMethod]
+        public void testCreateSearchPatientExistDictionary()
+        {
+            String patId = "1";
+            MedicalInformationManagementSystem.SearchPatient pa = new SearchPatient();
+            Assert.IsNotNull(pa);
+            Dictionary<String, String> dict = pa.createCheckPatientExistsDictionary(patId);
+            Assert.IsNotNull(dict);
+            Assert.IsTrue(dict.ContainsKey("@patientId"));
+            Assert.AreSame(dict["@patientId"], patId);
+        }
+
+        [TestMethod]
+        public void testCreateSearchPatientExistDataTable()
+        {
+            String patId = "1";
+            MedicalInformationManagementSystem.SearchPatient pa = new SearchPatient();
+            Assert.IsNotNull(pa);
+            Dictionary<String, String> dict = pa.createCheckPatientExistsDictionary(patId);
+            Assert.IsNotNull(dict);
+            Assert.IsTrue(dict.ContainsKey("@patientId"));
+            Assert.AreSame(dict["@patientId"], patId);
+            DataTable dat = new DataTable();
+           //dat = pa.checkPatientExists(dict);
+           
+           //Assert.IsNotNull(dat);
+           // Assert.AreEqual(dat.Rows[0][1],0);
+        }
+        
+        
+    }
+}
